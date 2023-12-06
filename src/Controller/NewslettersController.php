130864651e929065e027a43925a822cba7b4f171
shubampednekar <shubam.pednekar@sjinnovation.com>
2023-12-06 16:33:14 +0530

    test

--- src/Controller/NewslettersController.php
+++ src/Controller/NewslettersController.php
@@ -50,7 +50,7 @@ class NewslettersController extends AppController
         $resetNeeded = false;
         $search = null;
         $searchParam = $this->request->getQuery('search');
-        $newsletters = $this->Newsletters->find();
+        $newsletters = $this->Newsletters->find()->orderDesc('created');
         $this->set(compact('newsletters', 'resetNeeded', 'search'));
     }
 
@@ -130,7 +130,7 @@ class NewslettersController extends AppController
 
         if ($this->request->is(['patch', 'post', 'put'])) {
             $newsletter = $this->Newsletters->patchEntity($newsletter, $this->request->getData());
-            if ($this->Newsletters->save($video)) {
+            if ($this->Newsletters->save($newsletter)) {
                 
               $this->Flash->success(__('The Newsletter has been saved.'));
 
